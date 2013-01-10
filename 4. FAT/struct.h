6e9a03c38a086bc8317695093fcd828c7285105b
altaos <alishka.man@gmail.com>
2013-01-10 20:20:26 +0400

    New struct

--- 4. FAT/struct.h
+++ 4. FAT/struct.h
@@ -1,20 +1,22 @@
 #define CLUSTER_SIZE 8192 //байты
-#define ELEMENT_COUNT_IN_CATALOG 256
 #define CLUSTER_COUNT_FAT 1048576
 #define FAT_SIZE 4194304
 #define EOC 0x0FFFFFFF //конец цепочки кластеров
 #define NOT_USED 0 //кластер свободен
+#define ELEMENT_COUNT_IN_CATALOG 256
 
 struct File_Record
 {
-    char name[8]; //название
-    char extension[3]; //расширение
+    char name[256]; //название
     long first_cluster; //первый кластер
-    long file_size; //размер файла в байтах
-    time_t ctime; //дата создания
+    long size;//размер в байтах
+    ushort mode;//режим и тип файла
+    time_t ctime;//дата создания
     time_t mtime;//дата последнего изменения
-    char atribute; //атрибуты(r - read only, h - hidden, s - system, a - archive)
-    int isCatalog; //каталог или нет
+    time_t atime;//дата послденего доступа
+    ushort uid;//user id
+    ushort gid;//group id
+    long gen;//file generation number
 };
 
 
